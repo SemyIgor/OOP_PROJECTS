d7609c2b0d17ae391b8919f523db30827c522525
HenkSeed <adaner@inbox.ru>
2023-01-03 00:23:27 +0700

    Added some output lines into comments for visibility

--- Seminar01/MyPet/src/Main.java
+++ Seminar01/MyPet/src/Main.java
@@ -7,25 +7,25 @@ public class Main {
       bobDilan.setPet(bobic);
       bobic.setHuman(bobDilan);
 
-      System.out.println(bobDilan);
-      System.out.println(bobic);
+      System.out.println(bobDilan); // Human [hasMeal=true, hasToy=true]
+      System.out.println(bobic); // Pet [name=Бобик, isHungry=true, isTired=false, isSleeping=false,
+                                 // moodToPlay=false]
 
-      bobDilan.playWithPet(bobic);
+      bobDilan.playWithPet(bobic); // Бобик рычит // Бобик не хочет играть
 
       // bobDilan has got some meal and he is trying to feed the pet
-      bobDilan.feedPet();
-      // System.out.println(bobic);
+      bobDilan.feedPet(); // Бобик наелся
 
       // bobDilan hasn't got some meal and he is trying to feed the pet
-      bobDilan.feedPet();
+      bobDilan.feedPet(); // Бобик рычит
 
       // bobDilan hasn't got some toy and he is trying to play with bobic
       bobDilan.setHasToy(false);
-      bobDilan.playWithPet(bobic);
+      bobDilan.playWithPet(bobic); // Бобик убегает // Бобик не хочет играть
 
       // bobDilan has got some toy and he is trying to play with bobic
       bobDilan.setHasToy(true);
-      bobDilan.playWithPet(bobic);
+      bobDilan.playWithPet(bobic); // Бобик играет
 
    }
 }
