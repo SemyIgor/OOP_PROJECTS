706de147eb6463f79da19f1fbdc4173d77a19d7a
HenkSeed <adaner@inbox.ru>
2023-01-25 19:32:42 +0700

    Added Ex000 of the Lecture03

---
+++ Lecture03/Ex000/src/Foo.java
@@ -0,0 +1,5 @@
+public abstract class Foo {
+   public abstract void m1();
+
+   public abstract void m2();
+}
---
+++ Lecture03/Ex000/src/Program.java
@@ -0,0 +1,35 @@
+public class Program {
+   public static void main(String[] args) {
+      Foo foo1 = new Foo() {
+
+         @Override
+         public void m1() {
+            System.out.println("m1");
+         }
+
+         @Override
+         public void m2() {
+            System.out.println("m2");
+         }
+      };
+      foo1.m1(); // m1
+      foo1.m2(); // m2
+
+      Foo foo2 = new Foo() {
+
+         @Override
+         public void m1() {
+            System.out.println("m1>>>");
+         }
+
+         @Override
+         public void m2() {
+            System.out.println("m2>>>");
+
+         }
+      };
+
+      foo2.m1(); // m1>>>
+      foo2.m2(); // m2>>>
+   }
+}
