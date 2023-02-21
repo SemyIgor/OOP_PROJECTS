a3b1d8d8a72bd793142546b45d95170fdab8c95d
HenkSeed <adaner@inbox.ru>
2023-02-21 14:27:36 +0700

    Deleted old project

--- Seminar07/FilmLib/src/Service/IDataService.java
+++
@@ -1,9 +0,0 @@
-package Service;
-
-import java.util.ArrayList;
-
-import Models.Movie;
-
-public interface IDataService {
-   ArrayList<Movie> getAllMovies();
-}
--- Seminar07/FilmLib/src/Service/IRentService.java
+++
@@ -1,9 +0,0 @@
-package Service;
-
-import Models.UserRequest;
-
-public interface IRentService {
-   void processRequest(UserRequest request);
-
-   void returnMovie(UserRequest request);
-}
